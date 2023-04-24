90d86a08f11c2a795e9e1b37019430d14bf6e75b
Asher <124010300+Asta-Bots@users.noreply.github.com>
2023-04-24 21:00:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,7 @@ if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
   git clone https://github.com/Asta-Bots/Private-Auto-Movie-filter-bot 
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
-fi
+
 cd /Private-Auto-Movie-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
